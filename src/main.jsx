ddbd19c255f7922f5be08443cb16ae6d5b524fa3
VIMALUX <99176508+Vimalux@users.noreply.github.com>
2026-07-27 16:53:33 +0200

    Enable V31 manual project builder on preview branch

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import AppV31 from "./AppV31.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <AppV31 />
   </React.StrictMode>
 );
