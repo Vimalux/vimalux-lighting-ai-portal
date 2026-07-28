657492dcff68d14c5a699920c89da7dd75453949
VIMALUX <99176508+Vimalux@users.noreply.github.com>
2026-07-28 07:52:10 +0200

    Enable Planner-aligned typography and AI recommendation feedback

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AppV31Complete from "./AppV31Complete.jsx";
+import "./v31RuntimeEnhancements.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
