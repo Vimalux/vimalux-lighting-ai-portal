01bf8cd3e24ef692ae5fac418ce549f51d673919
VIMALUX <99176508+Vimalux@users.noreply.github.com>
2026-08-01 12:11:47 +0200

    Activate final bilingual Italian customer report

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import AppBusinessCaseV1 from "./AppBusinessCaseV1.jsx";
+import "./v35FinalCustomerReport.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
