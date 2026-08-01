1999c9ff606319aad274aecc23edfd82ccc10306
VIMALUX <99176508+Vimalux@users.noreply.github.com>
2026-08-01 12:34:13 +0200

    Activate production language and LCU completion patch

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import AppBusinessCaseV1 from "./AppBusinessCaseV1.jsx";
 import "./v35FinalCustomerReport.js";
+import "./v40ProductionFinishPatch.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
