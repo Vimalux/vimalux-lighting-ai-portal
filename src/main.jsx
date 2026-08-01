63cff4ac601996a54ee753402227710cd9c1af41
VIMALUX <99176508+Vimalux@users.noreply.github.com>
2026-08-01 10:57:24 +0200

    Activate final stable quotation engine

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import AppV32Projects from "./AppV32Projects.jsx";
+import AppFinalEngine from "./AppFinalEngine.jsx";
 import "./v31RuntimeEnhancements.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AppV32Projects />
+    <AppFinalEngine />
   </React.StrictMode>
 );
