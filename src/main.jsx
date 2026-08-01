37f3c6b119b65f149460373367ec83bb19d37432
VIMALUX <99176508+Vimalux@users.noreply.github.com>
2026-08-01 11:07:30 +0200

    Load proposal workspace polish

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import AppFinalEngine from "./AppFinalEngine.jsx";
 import "./v31RuntimeEnhancements.js";
+import "./v32ProposalPolish.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
