b91edf3e7de5a6d9be047d69e4347ef1172628d5
VIMALUX <99176508+Vimalux@users.noreply.github.com>
2026-09-13 13:22:00 +0200

    SAFE STAGING: load cashflow financing advisor

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,7 @@ import "./importedProductCategoryReconcile.js";
 import "./catalogueTabsRuntime.js";
 import "./defaultAssumptionsRuntime.js";
 import "./servicePeriodsRuntime.js";
+import "./financingAdvisorRuntime.js";
 import "./activeProjectContextRuntime.js";
 import "./intelligenceUiContinuityRuntime.js";
 import "./catalogueExpandedStateRuntime.js";
