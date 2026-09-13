855c639cacaf789e3d080cf42d2b5a92244cf36b
VIMALUX <99176508+Vimalux@users.noreply.github.com>
2026-09-13 13:49:10 +0200

    SAFE STAGING: load contract-bound chart horizon runtime

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ import "./catalogueTabsRuntime.js";
 import "./defaultAssumptionsRuntime.js";
 import "./servicePeriodsRuntime.js";
 import "./financingAdvisorRuntime.js";
+import "./contractChartHorizonRuntime.js";
 import "./activeProjectContextRuntime.js";
 import "./intelligenceUiContinuityRuntime.js";
 import "./catalogueExpandedStateRuntime.js";
